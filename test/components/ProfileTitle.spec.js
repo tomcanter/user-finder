bf551f8ba105bbf330919bbf9ca7a7ccc5df325f
Tom Canter <tomcanter@gmail.com>
2017-04-03 20:13:57 +0100

    Add ProfileTitle component

---
+++ test/components/ProfileTitle.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {
+  StyleSheetTestUtils,
+} from 'aphrodite';
+import ProfileTitle from 'components/ProfileTitle';
+
+describe('Component: ProfileTitle', () => {
+
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it('should render', () => {
+    const component = renderer.create(
+      <ProfileTitle
+        name="Tom"
+        username="tomcanter"
+        userLink="http://tomcanter.io"
+      />
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+});
